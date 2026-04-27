0675a6f91dce9d3d6e5da541dcab58c1e84fb21f
bivashgoessohard <bivash53@gmail.com>
2026-04-27 14:13:17 +0530

    Solved problem number 11 : Write a program in C for a 2D array of size 3x3 and print the matrix.

---
+++ Module-1/11.c
@@ -0,0 +1,43 @@
+/*
+11. Write a program in C for a 2D array of size 3x3 and print the matrix.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define ROWS 3
+#define COLS 3
+
+int main() {
+    // Dynamically allocate memory
+    int (*arr)[COLS] = malloc(ROWS * sizeof(*arr));
+
+    // Check memory allocation
+    if (arr == NULL) {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+
+    // Take input from user
+    printf("====Enter the elements of 3 x 3 matrix====\n");
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            printf("Enter element [%d] [%d]: ", i+1, j+1);
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+
+    printf("\n");
+    printf("\n");
+    // Print the matrix
+    printf("====The Matrix (3 x 3)====\n");
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            printf("%d \t", arr[i][j]);
+        }
+        printf("\n");
+        printf("\n");
+    }
+    printf("\n");
+
+}
