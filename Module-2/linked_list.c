5746ada14452aec94ab62a23df7e18bba2ab70ad
bivashgoessohard <bivash53@gmail.com>
2026-05-26 12:23:36 +0530

    Coded delete node functions and add at any position

--- Module-2/linked_list.c
+++ Module-2/linked_list.c
@@ -8,8 +8,164 @@ typedef struct Node {
 } Node;
 
 
-//Input funtion: at any point e.g., first, last, n-th position
-// TODO Tomorrow
+//Add funtion: at any point e.g., first, last, n-th position
+// Add at first 
+void addFirst(Node **head, int element) {
+    if (head == NULL)
+        return;
+
+    Node *newNode = malloc(sizeof(Node));
+
+    if(newNode == NULL) {
+        printf("Memory allocation failed!\n");
+        return;
+    }
+
+    newNode->data = element;
+    newNode->next = *head;
+    *head = newNode;
+}
+
+// Add at last
+void addLast(Node **head, int element) {
+    Node *newNode = malloc(sizeof(Node));
+
+    if (newNode == NULL) {
+        printf("Memory allocation failes.\n");
+        return;
+    }
+
+    newNode->next = NULL;
+    newNode->data = element;
+
+    // handle case where empty list is passed
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    Node *ptr = *head;
+
+    while(ptr->next != NULL) {
+        ptr = ptr->next;
+    }
+
+    ptr->next = newNode;
+}
+
+// Add k-th Node
+void addNode(Node **head, int element, size_t position) {
+
+    if (position < 1) {
+        printf("Invalid position!\n");
+        return;
+    }
+
+    // If postition is 1 
+    if (position == 1) {
+        addFirst(head, element);
+        return;
+    }
+
+    Node *ptr;
+    Node *newNode = malloc(sizeof(Node));
+
+    if (newNode == NULL) {
+        printf("Memory allocation failed!\n");
+        return;
+    }
+
+    newNode->data = element;
+    newNode->next = NULL;
+
+    if (*head == NULL) {
+        return;
+    }
+
+    ptr = *head;
+
+    for (size_t i = 1; i < position - 1; i++) {
+        if (ptr == NULL) {
+            printf("Position out of range!\n");
+            free(newNode);
+            return;
+        }
+        ptr = ptr->next;
+    }
+
+    newNode->next = ptr->next;
+    ptr->next = newNode;
+}
+
+
+// ------------Delete operations------------//
+// Delete first
+void deleteFirst(Node **head) {
+    // if head is empty
+    if (*head == NULL) {
+        printf("List is empty!\n");
+        return;
+    }
+
+    Node *temp = *head;
+    *head = (*head)->next;
+    free(temp);
+}
+
+// Delete last node
+void deleteLast(Node **head) {
+    if (*head == NULL) {
+        printf("List is empty!\n");
+        return;
+    }
+
+    // Single-node list handle
+    if ((*head)->next == NULL) {
+        free(*head);
+        *head = NULL;
+        return;
+    }
+
+    Node *curr = *head;
+    Node *prev = NULL;
+
+    while (curr->next != NULL) {
+        prev = curr;
+        curr = curr->next;
+    }
+    prev->next = NULL;
+    int data = curr->data;
+    free(curr);
+    printf("Deleted %d\n", data);
+}
+
+// Delete at any position
+void deleteNode(Node **head, size_t position) {
+    if (position < 1) {
+        printf("Invalid position!\n");
+        return;
+    }
+    if (*head == NULL) {
+        printf("List is empty!");
+    }
+
+    Node *curr = *head;
+
+    
+    if ((*head)->next == NULL) {
+        free(*head);
+    } else {
+        size_t count = 1;
+        while (count < position - 1) {
+            curr = curr->next;
+            count++;
+        }
+        Node *temp = curr->next;
+        curr->next = curr->next->next;
+        free(temp);
+        
+    }
+}
 
 // Reverse operation
 Node *reverseList(Node **head) {
@@ -98,6 +254,28 @@ int main(void) {
     reverseList(&head);
 
     // Call traverse function after reversing
+    traverse(head);
+
+    // Call add first element function
+    addFirst(&head, 0);
+
+    traverse(head);
+
+    // Call add last node
+    addLast(&head, 67);
+
+    traverse(head);
+
+    // Add at position 3
+    addNode(&head, 7, 3);
+    traverse(head);
+
+    // Call delete first Node
+    deleteFirst(&head);
+    traverse(head);
+
+    // Delete last
+    deleteNode(&head, 3);
     traverse(head);
     
     // Free memory
